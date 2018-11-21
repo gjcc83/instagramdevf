e928d2e9d795b4a366a112df62ce9d22318a91fc
gjcc83 <gilberto.carranzacastro@gmail.com>
2018-11-20 21:39:11 -0700

    Se agrega de imagenes

---
+++ src/schemas/Images.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ImageSchema = new Schema({
+
+    likes_quantity: {
+        type:Number,
+        default: 0
+    },
+    comment_id: {
+        type:Schema.Types.ObjectId, ref: "comments"
+    },
+    url: {
+        type:String,
+        required:true
+    },
+    description: {
+        type:String,
+        required:true
+    },
+    is_active: {
+        type:Boolean,
+        default:true
+    }
+}, {'collection': 'imagesInstagram', timestamps: true})
+
+module.exports = mongoose.model('imagesInstagram', ImageSchema)
