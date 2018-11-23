900aef2a939871aece689139de484cc5f7920a9b
Carlos Toscano <lic.carlostoscano@gmail.com>
2018-11-22 21:36:20 -0700

    Ajuste al crear token

--- src/utils/createToken.js
+++ src/utils/createToken.js
@@ -1,16 +1,14 @@
 const jwt = require('jsonwebtoken');
-const {SECRET_KEY} = require('../const');
+const { SECRET_KEY } = require('./../const');
 
-module.exports = (user) => {
-
+module.exports = user => {
     const payload = {
-        id : user.id,
-        email : user.email,
-        first_name : user.first_name
+        id: user._id,
+        email: user.email,
+        first_name: user.first_name
     };
 
     let token = jwt.sign(payload, SECRET_KEY);
 
     return token;
-
-};
+}
