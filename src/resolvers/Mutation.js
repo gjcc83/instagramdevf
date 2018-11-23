e486cdb8bb5ab33b2ea1fe9a7dee9f233e3b7f47
Carlos Toscano <lic.carlostoscano@gmail.com>
2018-11-22 21:38:14 -0700

    Ajustes en modelo de usuario

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,23 +1,21 @@
-const Users = require('../schemas/Users');
-const createToken = require('../utils/createToken');
-const comparePasswords = require('../utils/comparePasswords');
+const Users = require('./../schemas/Users');
+const createToken = require('./../utils/createToken');
+const comparePasswords = require('./../utils/comparePasswords');
 
 function signup(_, args, context, info) {
-
-    return Users.create(args.data).then((user) => {
-        
+    return Users.create(args.data).then(user => {
         let token = createToken(user);
-        return { token:token };
 
-    }).catch((err) => {
-        throw new Error(err);
+        return { token };
+    }).catch(err => {
+        throw err;
     });
 }
 
 function login(_, args, context, info) {
     return comparePasswords(args.email, args.password)
-        .then((token) => { return { token }; })
-        .catch((err) => { throw err; });
+        .then(token => { return { token } } )
+        .catch(err => { throw err });
 }
 
 module.exports = {
