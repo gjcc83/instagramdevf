c56c22a0d4cf255db37744cff1df266550650f63
gjcc83 <gilberto.carranzacastro@gmail.com>
2018-12-12 23:05:20 -0700

    Se elimina llave para stripe

--- src/const.js
+++ src/const.js
@@ -1,4 +1,3 @@
 module.exports = {
-    SECRET_KEY : process.env.SECRET_KEY || "@[=g3,8d]\\&fbb=-q]/hk%fg",
-    SECRET_KEY_STRIPE : process.env.SECRET_KEY_STRIPE || "sk_test_pYxjoqwyHCxAuTBSg5KmQFCK"
+    SECRET_KEY : process.env.SECRET_KEY || "@[=g3,8d]\\&fbb=-q]/hk%fg"
 };
